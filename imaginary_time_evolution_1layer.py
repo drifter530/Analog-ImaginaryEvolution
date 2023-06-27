26fc47f5c2e4ac8ac3482954ac290ee8252a1478
Song Jiaxing <pkujiaxing@stu.pku.edu.cn>
2023-06-27 23:07:04 +0800

    0627_1

--- imaginary_time_evolution_1layer.py
+++ imaginary_time_evolution_1layer.py
@@ -21,18 +21,18 @@ def multikron(*args):
         ret = np.kron(ret, op)
     return ret
 
-#qubit数，基底数，采样点数,基底类型
-n_qubit=4
-n_basis=3
-n_sample=10
-basis='legendre' 
-
 #保存图片的标签
 label=''
 
+#qubit数，基底数，采样点数,基底类型
+n_qubit=2
+n_basis=7
+n_sample=5
+basis='legendre' 
+
 #pulse总时长，Learning rate，epoch数
-Ts=[4*np.pi]
-lr=0.2
+Ts=[4*np.pi,6*np.pi,8*np.pi,10*np.pi]
+lr=0.3
 epoches=100
 
 #随机初始量子态
@@ -40,12 +40,19 @@ psi0=np.random.rand(2**n_qubit)
 psi0=psi0/np.linalg.norm(psi0)
 
 #控制Hamiltonian
-Hsys=0
-Hs=[multikron(X,I,I,I),multikron(I,X,I,I),multikron(I,I,X,I),multikron(I,I,I,X),multikron(Z,Z,I,I),multikron(I,Z,Z,I),multikron(I,I,Z,Z),multikron(Z,I,I,Z)]
+J12=0.5
+Hsys=0.3/2*(np.kron(I,I)-np.kron(Z,I))+0.5/2*(np.kron(I,I)-np.kron(I,Z))+J12*(np.kron(Y,Y)+np.kron(X,X))/4
+#Hs=[multikron(X,I,I,I),multikron(I,X,I,I),multikron(I,I,X,I),multikron(I,I,I,X),multikron(Z,Z,I,I),multikron(I,Z,Z,I),multikron(I,I,Z,Z),multikron(Z,I,I,Z)]
+Hs=[multikron(X,I),multikron(I,X)]
 n_pulse=len(Hs)
 
 #目标Hamiltonian
-Hc=np.array([])
+a0=-1.0524
+a1=-0.0113
+a2=0.1809
+a3=-0.3979
+a4=0.3979
+Hc=a0*np.kron(I,I)+a1*np.kron(Z,Z)+a2*np.kron(X,X)+a3*np.kron(Z,I)+a4*np.kron(I,Z)
 
 legendre_ps=[legendre(j) for j in range(n_basis)]
 
@@ -116,6 +123,7 @@ def natural_gradient():
                             z+=(du1*du2*np.conjugate(psi_it[i1][t1])@psi_it[i2][t2]).real
 
                     M[i1*n_basis+j1][i2*n_basis+j2]=z*(T**2)/(n_sample**2)
+                    
 
 
     psi_f=trotter(0,T,psi0)
@@ -128,13 +136,65 @@ def natural_gradient():
 
             z=0
             for t in range(n_sample):
-                du=0.5*(1+u(i,ti[t]))*(1-u(i,ti[t]))*legendre[j]((2*t-n_sample+1)/n_sample)
+                du=0.5*(1+u(i,ti[t]))*(1-u(i,ti[t]))*legendre_ps[j]((2*t-n_sample+1)/n_sample)
+                z+=(du*np.conjugate(psi_f)@Hc@psi_it[i][t]).imag
+            C[i*n_basis+j]=-np.mean(z)*T/n_sample
+
+    #取M的广义逆矩阵
+
+    return np.linalg.pinv(M,rcond=0.001)@C
+
+def natural_gradient_random():
+    #计算自然梯度： M Delta_L =C
+    
+    #采样时间（等间隔）
+    ti=np.linspace(T/(2*n_sample),T*(2*n_sample-1)/(2*n_sample),n_sample)
+
+    #对于不同的控制函数和采样时间，计算|psi_it>。注意到对于不同基函数并不需要重复计算。
+    psi_it=np.zeros((n_pulse,n_sample,2**n_qubit),dtype=complex)
+    for i in range(n_pulse):
+        for t in range(n_sample):
+            psi=trotter(0,ti[t],psi0)
+            psi=Hs[i]@psi
+            psi=trotter(ti[t],T,psi)
+            psi_it[i][t]=psi
+
+    M=np.zeros((n_pulse*n_basis,n_pulse*n_basis))
+    for i1 in range(n_pulse):
+        for j1 in range(n_basis):
+            for i2 in range(n_pulse):
+                for j2 in range(n_basis):
+
+                    #计算M矩阵矩阵元
+                    #矩形积分
+                    #M_ij=sum(t1,t2) du_i(t1)du_j(t2) Re[<psi_it(t1)|psi_it(t2)>]
+
+                    z=0
+                    for t1 in range(n_sample):
+                        for t2 in range(n_sample):
+                            du1=0.5*(1+u(i1,ti[t1]))*(1-u(i1,ti[t1]))*legendre_ps[j1]((2*t1-n_sample+1)/n_sample)
+                            du2=0.5*(1+u(i2,ti[t2]))*(1-u(i2,ti[t2]))*legendre_ps[j2]((2*t2-n_sample+1)/n_sample)
+                            z+=(du1*du2*np.conjugate(psi_it[i1][t1])@psi_it[i2][t2]).real
+
+                    M[i1*n_basis+j1][i2*n_basis+j2]=z*(T**2)/(n_sample**2)
+
+
+    psi_f=trotter(0,T,psi0)
+    C=np.zeros(n_pulse*n_basis)
+    for i in range(n_pulse):
+        for j in range(n_basis):
+
+            #计算C向量
+            #C_i=-sum(t) du_i(t) Im[<psi_f|Hc|psi_it(t)>]
+
+            z=0
+            for t in range(n_sample):
+                du=0.5*(1+u(i,ti[t]))*(1-u(i,ti[t]))*legendre_ps[j]((2*t-n_sample+1)/n_sample)
                 z+=(du*np.conjugate(psi_f)@Hc@psi_it[i][t]).imag
             C[i*n_basis+j]=-np.mean(z)*T/n_sample
 
     #取M的广义逆矩阵
     return np.linalg.pinv(M)@C
-
 
 lgloss=np.zeros(epoches)
 x=np.linspace(0,epoches,epoches)
@@ -144,24 +204,27 @@ E0=np.min(E)
 #随机初始参数
 coeff0=np.random.rand(n_basis*n_pulse)/10
 
-for T in Ts:
-    spectral_coeff=coeff0
-    for i in range(epoches):
+for n_sample in [3,5,7,10]:
+    for T in Ts:
+        spectral_coeff=coeff0
+        for i in range(epoches):
 
-        #更新参数
-        spectral_coeff=spectral_coeff+lr*natural_gradient()
+            #更新参数
+            spectral_coeff=spectral_coeff+lr*natural_gradient()
+            #print(spectral_coeff)
 
-        #计算loss function（对数形式）
-        psi_p=trotter(0,T,psi0)
-        lgloss[i]=np.log(abs(np.conjugate(psi_p)@Hc@psi_p-E0))/np.log(10)
-        print("T={}    epoch={}    lgloss={}".format(T,i,lgloss[i]))
+            #计算loss function（对数形式）
+            psi_p=trotter(0,T,psi0)
+            lgloss[i]=np.log(abs(np.conjugate(psi_p)@Hc@psi_p-E0))/np.log(10)
+            print("T={}    epoch={}    lgloss={}".format(T,i,lgloss[i]))
 
-    plt.plot(x,lgloss,label='T='+str(T))
+        plt.plot(x,lgloss,label='T='+str(T))
 
-#保存图片至log文件夹
-path="log\\figure_2\\NGD(lr={},(q,b,p)=({},{},{}),{}).png".format(lr,n_qubit,n_basis,n_pulse,label)
-plt.legend()
-plt.savefig(path)
+    #保存图片至log文件夹
+    path="log\\figure_0627\\NGD(lr={},(q,b,p,s)=({},{},{},{}),{}).png".format(lr,n_qubit,n_basis,n_pulse,n_sample,str(Ts))
+    plt.legend()
+    plt.savefig(path)
+    plt.cla()
 
 
 
