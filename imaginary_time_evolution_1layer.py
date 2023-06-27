2e459eb1c5d9caf5a20a1b8843e85a376b80ee8f
Song Jiaxing <pkujiaxing@stu.pku.edu.cn>
2023-06-27 11:04:17 +0800

    0626

--- imaginary_time_evolution_1layer.py
+++ imaginary_time_evolution_1layer.py
@@ -20,21 +20,21 @@ def multikron(*args):
         ret = np.kron(ret, op)
     return ret
 
-I4=multikron(I,I,I,I)
-
 n_qubit=4
-n_basis=5
+n_basis=3
+label=''
 
 basis='legendre'
-path='log\\figure\\NGD(lr=0.3,n=(2,2,3),H2).png'
 
-Ts=[2*np.pi]
-lr=0.02
-epoches=400
+Ts=[6*np.pi]
+lr=0.2
+epoches=100
 
 psi0=np.random.rand(2**n_qubit)
 psi0=psi0/np.linalg.norm(psi0)
 Hsys=0
+#Hsys=0.3/2*(np.kron(I,I)-np.kron(Z,I))+0.3/2*(np.kron(I,I)-np.kron(I,Z))+0.5*(np.kron(Y,Y)+np.kron(X,X))/4
+#Hs=[multikron(X,I),multikron(I,X)]
 Hs=[multikron(X,I,I,I),multikron(I,X,I,I),multikron(I,I,X,I),multikron(I,I,I,X),multikron(Z,Z,I,I),multikron(I,Z,Z,I),multikron(I,I,Z,Z),multikron(Z,I,I,Z)]
 n_pulse=len(Hs)
 
@@ -43,7 +43,7 @@ a1=-0.0113
 a2=0.1809
 a3=-0.3979
 a4=0.3979
-Hc=multikron(I,I,I,I)+a0*multikron(I,I,I,I)+a1*multikron(Z,Z,I,I)+a2*multikron(X,X,I,I)+a3*multikron(Z,I,I,I)+a4*multikron(I,Z,I,I)
+Hc=a0*multikron(I,I,I,I)+a1*(multikron(X,I,I,I)+multikron(I,X,I,I)+multikron(I,I,X,I)+multikron(I,I,I,X))+a2*(multikron(X,X,I,I)+multikron(X,I,X,I)+multikron(X,I,I,X)+multikron(I,X,X,I)+multikron(I,X,I,X)+multikron(I,I,X,X))+a3*(multikron(X,X,X,I)+multikron(X,X,I,X)+multikron(X,I,X,X)+multikron(I,X,X,X))+a4*multikron(X,X,X,X)
 
 def sigmoid(x):
     if x>0:
@@ -96,6 +96,8 @@ def computeMC(n_sample=10):
     psi_it=np.zeros((n_pulse,n_sample,2**n_qubit),dtype=complex)
 
     ti=np.linspace(T/(2*n_sample),T*(2*n_sample-1)/(2*n_sample),n_sample)
+
+
     for i in range(n_pulse):
         for t in range(n_sample):
             psi=trotter(0,ti[t],psi0)
@@ -103,32 +105,34 @@ def computeMC(n_sample=10):
             psi=trotter(ti[t],T,psi)
             psi_it[i][t]=psi
 
-    #print(psi_it)
+    
     
     M=np.zeros((n_pulse*n_basis,n_pulse*n_basis))
     for i1 in range(n_pulse):
         for j1 in range(n_basis):
             for i2 in range(n_pulse):
                 for j2 in range(n_basis):
-                    z=np.zeros(n_sample**2)
+                    z=0
                     for t1 in range(n_sample):
                         for t2 in range(n_sample):
                             du1=0.5*(1+u(i1,ti[t1]))*(1-u(i1,ti[t1]))*legendre(j1,(2*t1-n_sample+1)/n_sample)
                             du2=0.5*(1+u(i2,ti[t2]))*(1-u(i2,ti[t2]))*legendre(j2,(2*t2-n_sample+1)/n_sample)
-                            z[t1*n_sample+t2]=(du1*du2*np.conjugate(psi_it[i1][t1])@psi_it[i2][t2]).real
-                    M[i1*n_basis+j1][i2*n_basis+j2]=np.mean(z)*(T**2)
+                            z+=(du1*du2*np.conjugate(psi_it[i1][t1])@psi_it[i2][t2]).real
+
+                    M[i1*n_basis+j1][i2*n_basis+j2]=z*(T**2)/(n_sample**2)
+
 
     psi_f=trotter(0,T,psi0)
     C=np.zeros(n_pulse*n_basis)
     for i in range(n_pulse):
         for j in range(n_basis):
-            z=np.zeros(n_sample)
+            z=0
             for t in range(n_sample):
                 du=0.5*(1+u(i,ti[t]))*(1-u(i,ti[t]))*legendre(j,(2*t-n_sample+1)/n_sample)
-                z[t]=(du*np.conjugate(psi_f)@Hc@psi_it[i][t]).imag
-            C[i*n_basis+j]=-np.mean(z)*T
+                z+=(du*np.conjugate(psi_f)@Hc@psi_it[i][t]).imag
+            C[i*n_basis+j]=-np.mean(z)*T/n_sample
 
-
+    #print(M,C)
     return M,C
 
 t=time.localtime()
@@ -148,10 +152,11 @@ for T in Ts:
         spectral_coeff=spectral_coeff+lr*np.linalg.pinv(M)@C
         psi_p=trotter(0,T,psi0)
         dE[i]=np.log(abs(np.conjugate(psi_p)@Hc@psi_p-E0))/np.log(10)
-        print('T='+str(T)+"   epoch="+str(i)+"   lgloss="+str(dE[i]))
+        print("T={}    epoch={}    lgloss={}".format(T,i,dE[i]))
 
     plt.plot(x,dE,label='T='+str(T))
 
+path="log\\figure_2\\NGD(lr={},(q,b,p)=({},{},{}),{}).png".format(lr,n_qubit,n_basis,n_pulse,label)
 plt.legend()
 plt.savefig(path)
 
